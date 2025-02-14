5a34a715c0e40150a5ecdc2ea790a6e55fe1e0c2
Yasin Silavi <ys.silavi@gmail.com>
2025-02-15 01:24:20 +0330

    feat: add more messages

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -1,8 +1,9 @@
 package messages
 
 const (
-	GeneralSuccess = "عملیات با موفقیت انجام شد"
-	GeneralFailed  = "عملیات با شکست مواجه شد"
+	GeneralSuccess      = "عملیات با موفقیت انجام شد"
+	GeneralFailed       = "عملیات با شکست مواجه شد"
+	GeneralUnauthorized = "برای دسترسی به این بخش لطفا وارد شوید"
 
 	OTPInvalid = "کد وارد شده صحیح نیست"
 
@@ -11,4 +12,8 @@ const (
 	StoryNotCreated = "ثبت داستان موفقیت آمیز نبود"
 	StoryEdited     = "داستان با موفقیت ویرایش شد"
 	StoryDeleted    = "داستان با موفقیت حذف شد"
+
+	UserNotFound      = "کاربری با این شناسه یافت نشد"
+	UserEdited        = "اطلاعات شما با موفقیت ویرایش شد"
+	UserForbiddenName = "این نام برای انتخاب مجاز نیست"
 )
