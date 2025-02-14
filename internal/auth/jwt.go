f14ab2cb9700168ad5081f71279aacc569f7d0c2
Yasin Silavi <ys.silavi@gmail.com>
2025-02-15 01:23:04 +0330

    feat: add parse jwt and bearer token utils

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"errors"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -17,4 +19,52 @@ func GenerateJWTToken(id uint) (string, error) {
 	secret := os.Getenv("JWT_SECRET")
 
 	return token.SignedString([]byte(secret))
+}
+
+func ParseJWTToken(tokenString string) (map[string]interface{}, error) {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		return nil, errors.New("JWT secret is not set")
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Ensure the token is signed with the correct method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	if exp, ok := claims["exp"].(float64); ok {
+		if time.Now().Unix() > int64(exp) {
+			return nil, errors.New("token has expired")
+		}
+	} else {
+		return nil, errors.New("invalid exp claim")
+	}
+
+	return claims, nil
+}
+
+func ParseBearerToken(token string) (string, error) {
+	if token == "" {
+		return "", errors.New("authorization header is missing")
+	}
+
+	parts := strings.Split(token, " ")
+	if (len(parts) != 2) || strings.ToLower(parts[0]) != "bearer" {
+		return "", errors.New("invalid authorization header format")
+	}
+
+	return parts[1], nil
 }
