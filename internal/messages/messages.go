3479d4ea99d578f5e65920a354fb8c0365d14f21
Yasin Silavi <ys.silavi@gmail.com>
2025-02-19 23:28:50 +0330

    feat: update response messages

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -5,13 +5,16 @@ const (
 	GeneralFailed       = "عملیات با شکست مواجه شد"
 	GeneralUnauthorized = "برای دسترسی به این بخش لطفا وارد شوید"
 
-	OTPInvalid = "کد وارد شده صحیح نیست"
+	OTPInvalid  = "کد وارد شده صحیح نیست"
+	OTPTryAgain = "لطفا بعد از گذشت %d ثانیه مجددا تلاش کنید"
 
 	StoryNotFound   = "داستانی یافت نشد"
 	StoryCreated    = "داستان با موفقیت ثبت شد"
 	StoryNotCreated = "ثبت داستان موفقیت آمیز نبود"
 	StoryEdited     = "داستان با موفقیت ویرایش شد"
 	StoryDeleted    = "داستان با موفقیت حذف شد"
+	// StoryMinCharLimit = "داستان باید حداقل شامل ۲۵ حرف باشد"
+	// StoryMaxCharLimit = "داستان می‌تواند نهایتا شامل ۲۵۶ حرف باشد"
 
 	UserNotFound      = "کاربری با این شناسه یافت نشد"
 	UserEdited        = "اطلاعات شما با موفقیت ویرایش شد"
