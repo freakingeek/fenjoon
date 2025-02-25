5a7021a1d196bcef7ca0931056b239f5ceda12c1
Yasin Silavi <ys.silavi@gmail.com>
2025-02-25 17:36:08 +0330

    fix: change story user model

--- internal/models/story.go
+++ internal/models/story.go
@@ -10,7 +10,7 @@ type Story struct {
 	ID        uint           `gorm:"primaryKey" json:"id"`
 	Text      string         `gorm:"type:varchar(256);not null" json:"text"`
 	UserID    uint           `gorm:"not null" json:"userId"`
-	User      BaseUser       `gorm:"foreignKey:ID" json:"user"`
+	User      User           `gorm:"foreignKey:UserID" json:"user"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -18,12 +18,3 @@ type User struct {
 	UpdatedAt  time.Time      `json:"updatedAt"`
 	DeletedAt  gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
-
-type BaseUser struct {
-	ID         uint   `gorm:"primaryKey" json:"id"`
-	Phone      string `gorm:"varchar(11);<-:create" json:"phone"`
-	FirstName  string `gorm:"varchar(50);not null" json:"firstName"`
-	LastName   string `gorm:"varchar(50);not null" json:"lastName"`
-	Nickname   string `gorm:"varchar(50);not null" json:"nickname"`
-	IsVerified bool   `gorm:"default false" json:"isVerified"`
-}
