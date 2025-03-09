ac3193f89c84b8e3e1e7c008e98ae02fdddc7664
Yasin Silavi <ys.silavi@gmail.com>
2025-03-09 03:53:43 +0330

    chore: update cors domains

--- cmd/main.go
+++ cmd/main.go
@@ -22,7 +22,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://fenjoon.vercel.app", "http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
