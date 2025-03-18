7bd3d7effae156caf1208ea319cd3694c569f9ed
Yasin Silavi <ys.silavi@gmail.com>
2025-03-18 22:35:58 +0330

    feat: add push-token model

--- internal/database/database.go
+++ internal/database/database.go
@@ -23,7 +23,7 @@ func InitDB() {
 	}
 
 	// Auto-migrate models
-	err = db.AutoMigrate(&models.User{}, &models.Story{}, &models.Like{}, &models.Comment{}, &models.Share{})
+	err = db.AutoMigrate(&models.User{}, &models.Story{}, &models.Like{}, &models.Comment{}, &models.Share{}, &models.PushToken{})
 	if err != nil {
 		log.Fatal("failed to migrate database", err)
 	}
